121ac9605fce591b07a8f37d08c49bbbbc6fcc7d
gkishanrao <gkishanrao@gmail.com>
2025-09-05 08:35:51 -0700

    code changes

--- src/main/java/com/nirwantech/springboot_ecr_eks_deploy/Customer.java
+++ src/main/java/com/nirwantech/springboot_ecr_eks_deploy/Customer.java
@@ -6,11 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
     private String name;
     private String email;
+
     private String gender;
 }
--- src/main/java/com/nirwantech/springboot_ecr_eks_deploy/SpringbootEcrEksDeployApplication.java
+++ src/main/java/com/nirwantech/springboot_ecr_eks_deploy/SpringbootEcrEksDeployApplication.java
@@ -1,18 +1,28 @@
 package com.nirwantech.springboot_ecr_eks_deploy;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
 @RestController
-public class SpringbootEcrEksDeployApplication {
+public class SpringbootEcrEksDeployApplication  {
 
+
+
+	@PostConstruct
+	public void initLogin(){
+		System.out.print("POSTContractor login executed! \n");
+	}
 
 	@GetMapping("/hello")
 	public String message(){
@@ -22,7 +32,7 @@ public class SpringbootEcrEksDeployApplication {
 
 	@GetMapping("/getCustomers")
 	public List<Customer> getCustomer(){
-		return Stream.of(new Customer("Kishan","kishanrao@gmail.com","Male"),
+		return  Stream.of(new Customer("Kishan","kishanrao@gmail.com","Male"),
 				new Customer("Sahasra","sahara@gmail.com","Female"),
 				new Customer("Sunitha","sunitha@gmail.com","Female"),
 				new Customer("Jyodeep","jyodeep@gmail.com","Male"),
@@ -32,7 +42,16 @@ public class SpringbootEcrEksDeployApplication {
 
 
 	public static void main(String[] args) {
+		System.out.print("run method print");
+
 		SpringApplication.run(SpringbootEcrEksDeployApplication.class, args);
+
+
+
+
 	}
+
+
+
 
 }
